d2c19046aa3b0705a08eaa24f16c4577f42db77d
prohorp <usoltsev05@mail.ru>
2024-04-07 22:19:53 +0800

    Linux Debian 12, Clion , c++ (Debian 12.2.0-14) 12.2.0

---
+++ move.cpp
@@ -0,0 +1,9 @@
+//
+// Created by user on 4/7/24.
+//
+#include <iostream>
+#include "move.h"
+
+void todo_move_me_out() {
+    std::cout << "This function should be located in the separated source file" << std::endl;
+}
---
+++ move.h
@@ -0,0 +1,10 @@
+//
+// Created by user on 4/7/24.
+//
+
+#ifndef OTUS_CPP_BASIC_2024_03_USOLTSEVPETR_MOVE_H
+#define OTUS_CPP_BASIC_2024_03_USOLTSEVPETR_MOVE_H
+
+void todo_move_me_out();
+
+#endif //OTUS_CPP_BASIC_2024_03_USOLTSEVPETR_MOVE_H
