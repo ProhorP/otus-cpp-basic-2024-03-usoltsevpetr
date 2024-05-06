c7e10265c34274449f28e278adc21ed5ba798824
prohorp <usoltsev05@mail.ru>
2024-05-06 23:45:07 +0800

    hw04

---
+++ hw04/Ball.hpp
@@ -0,0 +1,38 @@
+#pragma once
+#include "Painter.hpp"
+#include "Point.hpp"
+#include "Velocity.hpp"
+
+class Ball {
+public:
+
+     inline Ball(const double Radius, const Color & BallColor) : Radius(Radius), BallColor(BallColor) {
+          Mass = M_PI * std::pow(Radius, 3) * 4 / 3;
+     }
+     inline Ball(const Ball &other) {
+          Radius = other.Radius;
+          Mass = other.Mass;
+          BallColor = other.BallColor;
+     }
+     inline Ball &operator=(const Ball &other){
+          Radius = other.Radius;
+          Mass = other.Mass;
+          BallColor = other.BallColor;
+          return *this;
+     }
+     inline ~Ball()= default;
+
+    void setVelocity(const Velocity& velocity);
+    Velocity getVelocity() const;
+    void draw(Painter& painter) const;
+    void setCenter(const Point& center);
+    Point getCenter() const;
+    double getRadius() const;
+    double getMass() const;
+  private:
+    Velocity BallVelocity;
+    double Radius;
+    double Mass;
+    Point Center;
+    Color BallColor;
+};
