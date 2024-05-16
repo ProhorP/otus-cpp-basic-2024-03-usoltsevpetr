7bc1a77478e379da7d84fd458b563cf221b5bb79
prohorp <usoltsev05@mail.ru>
2024-05-17 00:03:01 +0800

    hw05

--- hw05/statistics.cpp
+++ hw05/statistics.cpp
@@ -102,9 +102,8 @@ class Std : public IStatistics {
         double mean_value = mean.eval();
 
         Mean mean_square;
-        for (auto& entry : line) {
+        for (auto& entry : line)
             mean_square.update(pow(entry - mean_value, 2));
-        }
 
         return sqrt(mean_square.eval());
     }
