9971cbfcc9ce66983591338f09cd2d4be7e1cd48
prohorp <usoltsev05@mail.ru>
2024-06-20 21:55:49 +0800

    hw07 все тесты

--- hw07/Widget.h
+++ hw07/Widget.h
@@ -9,14 +9,13 @@
 class Widget {
 
   public:
-    explicit inline Widget() : ptr{nullptr}, check{false}{};
-    explicit inline Widget(std::size_t *ptr) : ptr{ptr}, check{false}{};
+    explicit inline Widget() : ptr{nullptr}{};
+    explicit inline Widget(std::size_t *ptr) : ptr{ptr}{};
     Widget(const Widget& other);
     Widget& operator=(const Widget& other);
     ~Widget();
 
   private:
-    bool check;
     std::size_t *ptr; //non free
 };
 
