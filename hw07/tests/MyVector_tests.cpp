e0e3b066ce69c82ff873c5b8d5df03abf3066e7d
prohorp <usoltsev05@mail.ru>
2024-06-20 19:39:17 +0800

    hw07 MyVector test

--- hw07/tests/MyVector_tests.cpp
+++ hw07/tests/MyVector_tests.cpp
@@ -1,7 +1,7 @@
 #include "MyVector.hpp"
 #include <gtest/gtest.h>
 
-TEST(List, Empty) {
+TEST(List, Create) {
     // Arrange
     MyVector<int> list;
 
@@ -23,6 +23,177 @@ TEST(List, PushBack) {
 
     // Assert
     ASSERT_EQ(list.size(), count);
+}
+
+TEST(List, PushFront) {
+    // Arrange
+    const size_t count = 10;
+    MyVector<std::size_t> list;
+
+    // Act
+    for (std::size_t i = 0; i < count; ++i) {
+        list.push_back(i);
+    }
+
+    list.insert(0, 10);
+
+    // Assert
+    ASSERT_EQ(list.size(), count + 1);
+}
+
+TEST(List, PushMiddle) {
+    // Arrange
+    const size_t count = 10;
+    MyVector<std::size_t> list;
+
+    // Act
+    for (std::size_t i = 0; i < count; ++i) {
+        list.push_back(i);
+    }
+
+    list.insert(list.size() / 2, 20);
+
+    // Assert
+    ASSERT_EQ(list.size(), count + 1);
+}
+
+TEST(List, EraseBack) {
+    // Arrange
+    const size_t count = 10;
+    MyVector<std::size_t> list;
+
+    // Act
+    for (std::size_t i = 0; i < count; ++i) {
+        list.push_back(i);
+    }
+
+    list.erase(list.size() - 1);
+
+    // Assert
+    ASSERT_EQ(list.size(), count - 1);
+}
+
+TEST(List, EraseFront) {
+    // Arrange
+    const size_t count = 10;
+    MyVector<std::size_t> list;
+
+    // Act
+    for (std::size_t i = 0; i < count; ++i) {
+        list.push_back(i);
+    }
+
+    list.erase(0);
+
+    // Assert
+    ASSERT_EQ(list.size(), count - 1);
+}
+
+TEST(List, EraseMiddle) {
+    // Arrange
+    const size_t count = 10;
+    MyVector<std::size_t> list;
+
+    // Act
+    for (std::size_t i = 0; i < count; ++i) {
+        list.push_back(i);
+    }
+
+    list.erase(list.size() / 2);
+
+    // Assert
+    ASSERT_EQ(list.size(), count - 1);
+}
+
+TEST(List, GetBack) {
+    // Arrange
+    const size_t count = 10;
+    MyVector<std::size_t> list;
+
+    // Act
+    for (std::size_t i = 0; i < count; ++i) {
+        list.push_back(i);
+    }
+
+    // Assert
+    ASSERT_EQ(list[list.size() - 1], 9);
+}
+
+TEST(List, GetFront) {
+    // Arrange
+    const size_t count = 10;
+    MyVector<std::size_t> list;
+
+    // Act
+    for (std::size_t i = 0; i < count; ++i) {
+        list.push_back(i);
+    }
+
+    // Assert
+    ASSERT_EQ(list[0], 0);
+}
+
+TEST(List, GetMiddle) {
+    // Arrange
+    const size_t count = 10;
+    MyVector<std::size_t> list;
+
+    // Act
+    for (std::size_t i = 0; i < count; ++i) {
+        list.push_back(i);
+    }
+
+    // Assert
+    ASSERT_EQ(list[list.size() / 2], 5);
+}
+
+TEST(List, GetSize) {
+    // Arrange
+    const size_t count = 10;
+    MyVector<std::size_t> list;
+
+    // Act
+    for (std::size_t i = 0; i < count; ++i) {
+        list.push_back(i);
+    }
+
+    // Assert
+    ASSERT_EQ(list.size(), 10);
+}
+
+TEST(List, Copy) {
+    // Arrange
+    const size_t count = 10;
+    MyVector<std::size_t> list;
+
+    // Act
+    for (std::size_t i = 0; i < count; ++i) {
+        list.push_back(i);
+    }
+
+    MyVector<std::size_t> list_copy(list);
+
+    // Assert
+    for (std::size_t i = 0; i < count; ++i) {
+        ASSERT_EQ(list[i], list_copy[i]);
+    }
+}
+
+TEST(List, Move) {
+    // Arrange
+    const size_t count = 10;
+    MyVector<std::size_t> list;
+
+    // Act
+    for (std::size_t i = 0; i < count; ++i) {
+        list.push_back(i);
+    }
+
+    MyVector list_move = std::move(list);
+
+    // Assert
+    ASSERT_EQ(list.size(), 0);
+    ASSERT_EQ(list_move.size(), 10);
 }
 
 int main(int argc, char** argv) {
